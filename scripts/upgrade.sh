f47ca3a3076b574e3d6ce72ec7636ad959e87642
Tolu Aina <7848930+toluaina@users.noreply.github.com>
2020-12-01 20:46:54 +0100

    added script for upgrading requirements

---
+++ scripts/upgrade.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+pip-compile --output-file=requirements/dev.txt requirements/dev.in --upgrade
+pip-compile --output-file=requirements/prod.txt requirements/prod.in --upgrade
+pip-compile --output-file=requirements/test.txt requirements/test.in --upgrade
