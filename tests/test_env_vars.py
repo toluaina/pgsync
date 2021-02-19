1ef43ee248fcdad7739ed7a8096a5671c7446bc5
Tolu Aina <7848930+toluaina@users.noreply.github.com>
2021-02-19 23:10:10 +0100

    bump version number

---
+++ tests/test_env_vars.py
@@ -0,0 +1,10 @@
+"""Env vars tests."""
+import pytest
+
+
+@pytest.mark.usefixtures('table_creator')
+class TestEnvVars(object):
+    """Env vars tests."""
+
+    def test_env_vars(self):
+        pass
