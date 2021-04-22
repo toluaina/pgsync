7b7bd4ee0ac36e8cfd5a87ed066e7297b58bc8b4
Tolu Aina <7848930+toluaina@users.noreply.github.com>
2021-04-22 10:52:09 +0100

    fixed test_get_elasticsearch_client

--- tests/test_elastichelper.py
+++ tests/test_elastichelper.py
@@ -64,11 +64,15 @@ class TestElasticsearchHelper(object):
                     'pgsync.elastichelper.AWS4Auth',
                     return_value='foo',
                 ):
-                    get_elasticsearch_client(url)
-                    mocker_elasticsearch.assert_called_once_with(
-                        hosts=[{'host': url, 'port': 443}],
-                        http_auth=ANY,
-                        use_ssl=True,
-                        verify_certs=True,
-                        connection_class=RequestsHttpConnection,
-                    )
+                    with mock.patch(
+                        'pgsync.elastichelper.boto3',
+                        return_value=MagicMock(),
+                    ):
+                        get_elasticsearch_client(url)
+                        mocker_elasticsearch.assert_called_once_with(
+                            hosts=[{'host': url, 'port': 443}],
+                            http_auth=ANY,
+                            use_ssl=True,
+                            verify_certs=True,
+                            connection_class=RequestsHttpConnection,
+                        )
