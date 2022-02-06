f9b7693fb0e1ab55050738055963209162332cb7
Tolu Aina <7848930+toluaina@users.noreply.github.com>
2022-02-06 23:28:18 +0100

    patch ElasticHelper to avoid leaving open socket connections in helper

--- tests/test_helper.py
+++ tests/test_helper.py
@@ -21,9 +21,10 @@ class TestHelper(object):
             mock_db.assert_called_once()
         mock_logger.warning.assert_not_called()
 
+    @patch("pgsync.sync.ElasticHelper")
     @patch("pgsync.helper.logger")
     @patch("pgsync.helper.get_config")
-    def test_teardown_without_drop_db(self, mock_config, mock_logger):
+    def test_teardown_without_drop_db(self, mock_config, mock_logger, mock_es):
         mock_config.return_value = "tests/fixtures/schema.json"
         with patch("pgsync.sync.Sync") as mock_sync:
             mock_sync.truncate_schemas.side_effect = sa.exc.OperationalError
