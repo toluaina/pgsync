c3f16249a24ae595006c381be00db990b6f45982
Tolu Aina <7848930+toluaina@users.noreply.github.com>
2022-12-12 23:11:24 +0100

    updatd requirements

---
+++ scripts/clear_disk_usage.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+export $(grep -v '^#' .env | xargs)
+
+curl -XPUT -H "Content-Type: application/json" $ELASTICSEARCH_HOST:$ELASTICSEARCH_PORT/_cluster/settings -d '{ "transient": { "cluster.routing.allocation.disk.threshold_enabled": false } }'
+curl -XPUT -H "Content-Type: application/json" $ELASTICSEARCH_HOST:$ELASTICSEARCH_PORT/_all/_settings -d '{"index.blocks.read_only_allow_delete": null}'
+curl -XPUT -H "Content-Type: application/json" $ELASTICSEARCH_HOST:$ELASTICSEARCH_PORT/_all/_settings -d '{"index.mapping.total_fields.limit": 3000}'
