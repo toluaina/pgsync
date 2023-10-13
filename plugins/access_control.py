3b82fe8523d67433aae0609ce7d7aaa0feccff56
Benjamin Baffy <benjamin@kicker.cloud>
2023-10-13 13:26:03 +0300

    create plugins samples folder

--- plugins/access_control.py
+++ plugins/access_control.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import Dict, List, Literal, Optional, Set
 from pgsync import plugin
 
 
@@ -6,13 +6,17 @@ class FullnamePlugin(plugin.Plugin):
     name = "AccessControl"
 
     def transform(self, doc, **kwargs):
-        owner_group = doc.get("owner_group")
-        modify_shared_with_groups = doc.get("modify_shared_with_groups", list)
-        read_shared_with_groups = doc.get("read_shared_with_groups", list)
-        groups: List[int] = []
+        owner_group: Optional[int] = doc.get("owner_group_id")
+        modify_shared_with_groups: List[
+            Dict[Literal["group_id"], int]
+        ] = doc.get("modify_shared_with_groups", list)
+        read_shared_with_groups: List[
+            Dict[Literal["group_id"], int]
+        ] = doc.get("read_shared_with_groups", list)
+        groups: Set[int] = set()
         if owner_group is not None:
-            groups.append(owner_group)
-        groups.extend(modify_shared_with_groups)
-        groups.extend(read_shared_with_groups)
+            groups.add(owner_group)
+        groups.update([d["group_id"] for d in modify_shared_with_groups])
+        groups.update([d["group_id"] for d in read_shared_with_groups])
         doc["access_control_groups"] = groups
         return doc
