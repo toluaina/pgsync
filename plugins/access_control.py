65b8b5d08c06145e04813620fd421533d9b63cda
Benjamin Baffy <benjamin@kicker.cloud>
2023-10-13 14:08:25 +0300

    fix access control plugin

--- plugins/access_control.py
+++ plugins/access_control.py
@@ -1,22 +1,30 @@
-from typing import Dict, List, Literal, Optional, Set
+from typing import Any, Dict, List, Literal, Optional, Set
 from pgsync import plugin
 
 
 class FullnamePlugin(plugin.Plugin):
     name = "AccessControl"
 
-    def transform(self, doc, **kwargs):
-        owner_group: Optional[int] = doc.get("owner_group_id")
+    def transform(self, doc:Dict[str, Any], **kwargs):
+        m = "_modify_shared_with_groups"
+        index_name = ""
+        for k in doc.keys():
+            if m in k:
+                index_name = k[:-len(m)]
+                break
+        owner_group: Optional[int] = doc.pop("owner_group_id")
         modify_shared_with_groups: List[
             Dict[Literal["group_id"], int]
-        ] = doc.get("modify_shared_with_groups", list)
+        ] = doc.pop(f"{index_name}_modify_shared_with_groups", [])
         read_shared_with_groups: List[
             Dict[Literal["group_id"], int]
-        ] = doc.get("read_shared_with_groups", list)
+        ] = doc.pop(f"{index_name}_read_shared_with_groups", [])
         groups: Set[int] = set()
         if owner_group is not None:
             groups.add(owner_group)
-        groups.update([d["group_id"] for d in modify_shared_with_groups])
-        groups.update([d["group_id"] for d in read_shared_with_groups])
-        doc["access_control_groups"] = groups
+        if modify_shared_with_groups is not None:
+            groups.update([d["group_id"] for d in modify_shared_with_groups])
+        if read_shared_with_groups is not None:
+            groups.update([d["group_id"] for d in read_shared_with_groups])
+        doc["access_control_groups"] = list(groups)
         return doc
