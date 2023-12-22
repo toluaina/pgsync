f771ff781b29f069e8224d2a1e4e4b73005f0a5a
Tolu Aina <7848930+toluaina@users.noreply.github.com>
2023-12-22 14:56:31 +0100

    python 3.8 support for pre-commit

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -3,6 +3,6 @@
 set -e
 set -x
 
-isort --profile black --check-only bin demo examples pgsync plugins scripts tests
-black --check bin demo examples pgsync plugins scripts tests
-flake8 pgsync tests examples plugins demo
+isort --profile black --check-only bin/* demo examples pgsync plugins scripts tests
+black --check bin/* demo examples pgsync plugins scripts tests
+flake8 pgsync tests examples plugins demo bin/*
