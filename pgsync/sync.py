de812696d9346e4a5a5a744fa50f9696daddcbef
Miguel Paolino <mpaolino@gmail.com>
2024-06-11 14:36:31 -0300

    Fix poll_db crash when notifications payload indeces is None (#537)
    * Checks payload indices key non-null value before attempting to iterate during syncs
    
    * adding timeout parameter to search client in aws hosted opensearch and elasticsearch
    
    * Revert "adding timeout parameter to search client in aws hosted opensearch and elasticsearch"
    
    This reverts commit cb148294e3039b6b302558a84affc3629bee69ff.

--- pgsync/sync.py
+++ pgsync/sync.py
@@ -1123,7 +1123,7 @@ class Sync(Base, metaclass=Singleton):
                 notification: t.AnyStr = conn.notifies.pop(0)
                 if notification.channel == self.database:
                     payload = json.loads(notification.payload)
-                    if self.index in payload["indices"]:
+                    if payload["indices"] and self.index in payload["indices"]:
                         payloads.append(payload)
                         logger.debug(f"poll_db: {payload}")
                         self.count["db"] += 1
@@ -1145,7 +1145,7 @@ class Sync(Base, metaclass=Singleton):
             notification: t.AnyStr = self.conn.notifies.pop(0)
             if notification.channel == self.database:
                 payload = json.loads(notification.payload)
-                if self.index in payload["indices"]:
+                if payload["indices"] and self.index in payload["indices"]:
                     self.redis.push([payload])
                     logger.debug(f"async_poll: {payload}")
                     self.count["db"] += 1
