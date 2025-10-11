f2d40677b57adea89990fdd346645ec477430620
toluaina <7848930+toluaina@users.noreply.github.com>
2025-10-12 00:06:55 +0100

    skip node test

--- tests/test_node.py
+++ tests/test_node.py
@@ -116,6 +116,10 @@ class TestNode(object):
             ],
         }
 
+    @pytest.mark.skipif(
+        IS_MYSQL_COMPAT,
+        reason="Skipped because IS_MYSQL_COMPAT env var is set",
+    )
     def test_node(self, connection):
         pg_base = Base(connection.engine.url.database)
         node = Node(
